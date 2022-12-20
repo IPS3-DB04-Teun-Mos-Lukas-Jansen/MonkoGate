34fb5e9f7986679b472a01a022dc796923aa3bd4
TeunMos <teun.mos@gmail.com>
2022-12-20 14:32:46 +0100

    added https
    Co-authored-by: Lukas <LukasJansen100@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
-const express = require('express');
-const proxy = require('express-http-proxy');
+const express = require("express");
+const proxy = require("express-http-proxy");
+const https = require("https");
+const fs = require("fs");
+require("dotenv").config();
 
 const app = express();
-const port = 80;
+const port = 443;
 
 app.use("/api/userpref", proxy("http://localhost:8000"));
 app.use("/api/integration", proxy("http://localhost:81"));
+app.use(proxy("http://localhost:3000"));
 
-app.use(proxy('http://localhost:3000'));
-
-app.listen(port, () => {
-    console.log(`Monkogate listening at http://localhost:${port}`);
-    });
+https
+  .createServer(
+    {
+      key: fs.readFileSync(process.env.HTTPS_KEY),
+      cert: fs.readFileSync(process.env.HTTPS_CERT),
+    },
+    app
+  )
+  .listen(port, () => {
+    console.log(`Monkogate listening at https://localhost:${port}`);
+  });
